cc9167e31a4a7727e8412f766a03cb0747f7a043
Ulf Lilleengen <lulf@redhat.com>
2022-06-15 12:30:42 +0200

    Add more examples

---
+++ demos/other/lorawan.rs
@@ -0,0 +1,61 @@
+#![no_std]
+#![no_main]
+#![macro_use]
+#![allow(dead_code)]
+#![feature(generic_associated_types)]
+#![feature(type_alias_impl_trait)]
+
+use embassy_lora::stm32wl::*;
+use embassy_lora::LoraTimer;
+use embassy_stm32::dma::NoDma;
+use embassy_stm32::gpio::{Level, Output, Pin, Speed};
+use embassy_stm32::rng::Rng;
+use embassy_stm32::subghz::*;
+use embassy_stm32::{interrupt, pac, Peripherals};
+use lorawan::default_crypto::DefaultFactory as Crypto;
+use lorawan_device::async_device::{region, Device, JoinMode};
+use {defmt_rtt as _, panic_probe as _};
+
+fn config() -> embassy_stm32::Config {
+    let mut config = embassy_stm32::Config::default();
+    config.rcc.mux = embassy_stm32::rcc::ClockSrc::HSI16;
+    config.rcc.enable_lsi = true;
+    config
+}
+
+#[embassy::main(config = "config()")]
+async fn main(_spawner: embassy::executor::Spawner, p: Peripherals) {
+    unsafe { pac::RCC.ccipr().modify(|w| w.set_rngsel(0b01)) }
+
+    let ctrl1 = Output::new(p.PC3.degrade(), Level::High, Speed::High);
+    let ctrl2 = Output::new(p.PC4.degrade(), Level::High, Speed::High);
+    let ctrl3 = Output::new(p.PC5.degrade(), Level::High, Speed::High);
+    let rfs = RadioSwitch::new(ctrl1, ctrl2, ctrl3);
+
+    let radio = SubGhz::new(p.SUBGHZSPI, p.PA5, p.PA7, p.PA6, NoDma, NoDma);
+
+    let irq = interrupt::take!(SUBGHZ_RADIO);
+    static mut RADIO_STATE: SubGhzState<'static> = SubGhzState::new();
+    let radio = SubGhzRadio::new(unsafe { &mut RADIO_STATE }, radio, rfs, irq) };
+
+    let region = region::EU868::default().into();
+    let mut device: Device<_, Crypto, _, _> = Device::new(region, radio, LoraTimer, Rng::new(p.RNG));
+
+    defmt::info!("Joining LoRaWAN network");
+
+    // TODO: Adjust the EUI and Keys according to your network credentials
+    device
+        .join(&JoinMode::OTAA {
+            deveui: [0, 0, 0, 0, 0, 0, 0, 0],
+            appeui: [0, 0, 0, 0, 0, 0, 0, 0],
+            appkey: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        })
+        .await
+        .ok()
+        .unwrap();
+    defmt::info!("LoRaWAN network joined");
+
+    defmt::info!("Sending 'PING'");
+    device.send(b"PING", 1, false).await.ok().unwrap();
+    defmt::info!("Message sent!");
+}
